579737187f3e501d18c4f20c8c3f45c21ab213f6
Mateusz Derks <mateusz.derks@toptal.com>
2019-08-01 21:25:49 +0200

    Updates

---
+++ precache-manifest.2beace5d0940f49442810168ac8005f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1987dd0d3b683fee5663a8d64ff19f8f",
+    "url": "/food-time/index.html"
+  },
+  {
+    "revision": "6fab04608abdbc564fe4",
+    "url": "/food-time/static/css/main.50cf232f.chunk.css"
+  },
+  {
+    "revision": "bc9a487deb5ad8d2e9fd",
+    "url": "/food-time/static/js/2.a5c2e03b.chunk.js"
+  },
+  {
+    "revision": "6fab04608abdbc564fe4",
+    "url": "/food-time/static/js/main.776bd1d3.chunk.js"
+  },
+  {
+    "revision": "ecb44f2abd6dd9f365b2",
+    "url": "/food-time/static/js/runtime~main.8d9e5b06.js"
+  }
+]);
