c2460e9fcb30007431232940dca641c32fbd5ae8
Mateusz Derks <mateusz.derks@toptal.com>
2019-08-10 20:04:38 +0200

    Updates

--- precache-manifest.566ab7e968c7210c6061137e0dd4486a.js
+++ precache-manifest.566ab7e968c7210c6061137e0dd4486a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "81dde2d1b08241ad6576df29a039adce",
+    "revision": "73780ce411f3a3e64021212b5722c7a9",
     "url": "/food-time/index.html"
   },
   {
-    "revision": "4ba1d983d0d726aab69e",
+    "revision": "028632c517ede5b5cc0e",
     "url": "/food-time/static/css/main.bb4b19d8.chunk.css"
   },
   {
-    "revision": "2db0e316fe0cb5621ae8",
-    "url": "/food-time/static/js/2.54d15b2a.chunk.js"
+    "revision": "0fccb7465787e6e952d0",
+    "url": "/food-time/static/js/2.93ab4615.chunk.js"
   },
   {
-    "revision": "4ba1d983d0d726aab69e",
-    "url": "/food-time/static/js/main.537929d5.chunk.js"
+    "revision": "028632c517ede5b5cc0e",
+    "url": "/food-time/static/js/main.4f16ea6a.chunk.js"
   },
   {
     "revision": "ecb44f2abd6dd9f365b2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/food-time/precache-manifest.154b09e1eccbcf180995d9432033d578.js"
+  "/food-time/precache-manifest.566ab7e968c7210c6061137e0dd4486a.js"
 );
 
 self.addEventListener('message', (event) => {
